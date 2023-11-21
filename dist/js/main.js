2ba3b9051367f41266cb5991c8ff5b8239ef1d3d
Tofriev <t.voelkel@posteo,de>
2023-11-21 19:15:36 +0200

    added menu for medium and small screens

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,6 +23,42 @@ window.addEventListener('scroll', updateActiveLink);
 window.addEventListener('DOMContentLoaded', updateActiveLink);
 
 
-document.getElementById('hamburger-icon').addEventListener('click', function() {
-    document.body.classList.toggle('is-active');
-});
+
+
+
+
+// Select DOM Items
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+
+// Set Initial State of Menu 
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //set Menu State
+        showMenu = false;
+    }
+}
+
+
+
